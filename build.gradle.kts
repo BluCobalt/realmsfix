3080f9af39f27ae2c15a17db7430b4d9f8f9e0b3
GeorgeTR1 <35677090+GeorgeTR1@users.noreply.github.com>
2025-03-31 20:07:32 -0500

    Make mergedJar task show up in the gradlew tasks list

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,6 +89,9 @@ subprojects {
 }
 
 tasks.register<Jar>("mergedJar") {
+    group = "Build"
+    description = "Combines all versions into a single jar. Most common way to build the mod"
+
     archiveBaseName.set("realmsfix")
     destinationDirectory.set(file("$buildDir/mergedJars"))
 
